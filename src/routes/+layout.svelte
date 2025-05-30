917ce626eaad9b09a64eec7c07e2f39bc4b298cb
Sr-Kaio <srkaio26@proton.me>
2025-05-30 02:30:18 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+	
+	let { children } = $props();
+</script>
+
+<title>Yume Marrige :3</title>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<!-- main stuffy stuff -->
+<div class="w-screen h-screen flex flex-col">
+    <div class="h-full w-full flex items-center justify-center">
+        <div
+            class="rounded-(--radius) overflow-hidden border-2 shadow-lg dark:bg-background bg-light-background dark:shadow-text/10 shadow-light-text/25 dark:border-text/30 border-light-text/30 h-full w-full max-h-[512px] max-w-[924px]"
+        >
+            <!-- window header thingy -->
+            <div
+                class="w-full border-b-2 dark:border-b-text/30 border-b-light-text/30 flex justify-between item-center p-2"
+            >
+                <!-- window title -->
+                <div class="dark:text-text/50 text-light-text/50 font-bold">
+                    Yume Marrige Certificate Generator! X3
+                </div>
+
+                <!-- fake macos trafic lights -->
+                <div class="flex p-1 flex-end w-15 gap-1">
+                    <!-- svelte-ignore element_invalid_self_closing_tag -->
+                    <div
+                        class="aspect-square dark:bg-text/30 bg-light-text/30 rounded-full w-4"
+                    />
+                    <!-- svelte-ignore element_invalid_self_closing_tag -->
+                    <div
+                        class="aspect-square dark:bg-text/30 bg-light-text/30 rounded-full w-4"
+                    />
+                    <!-- svelte-ignore element_invalid_self_closing_tag -->
+                    <div
+                        class="aspect-square bg-light-primary/50 dark:bg-primary/50 rounded-full w-4"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- this is the footer -->
+    <div class="h-[90px]"></div>
+</div>
